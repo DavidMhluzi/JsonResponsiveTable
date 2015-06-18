099df2049ba20aa7a0c8516444971e2dbf750ead
DavidMhluzi <mhluzidavid@gmail.com>
2015-06-18 16:12:01 +0200

    []Initial commit of project

---
+++ JobsTable/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" Inherits="Default" Codebehind="Default.aspx.cs" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <meta charset="utf-8"/>   
+    <title>Job List</title>   
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet"/>   
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="http://cdn.datatables.net/1.10.2/css/jquery.dataTables.min.css"/></style>
+    <script type="text/javascript" src="http://cdn.datatables.net/1.10.2/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+<body style="margin:20px auto">
+    <form id="form1" runat="server">
+    <div class="container">
+         <div class="row header" style="text-align:center;color:green">
+        <h3>Jobs</h3>
+        </div>
+        <table id="myTable" class="table table-striped" >  
+        <thead>  
+          <tr>  
+            <th>Client</th>  
+            <th>Job Number</th>  
+            <th>Job Name</th>  
+            <th>Due</th>  
+            <th>Status</th>  
+          </tr>  
+        </thead>  
+        <tbody>  
+            <asp:PlaceHolder id="placeHolder" runat="server"></asp:PlaceHolder>
+        </tbody>  
+      </table>  
+    </div>
+    </form>
+</body>
+    <script>
+        $(document).ready(function () {
+            $('#myTable').dataTable();
+        });
+</script>
+</html>
---
+++ JobsTable/Default.aspx.cs
@@ -0,0 +1,94 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string json = getJobsContent("https://gist.githubusercontent.com/WillemLabu/34cfb50187ec334c48ee/raw/84f2ebf1c2343a23792d725ef3da4a9c61f10857/jobs.json");
+        var o = JObject.Parse(json);
+        
+        #region Data Table
+
+        DataTable dtJobList = new DataTable();
+        dtJobList.Columns.Add("Client", typeof(string));
+        dtJobList.Columns.Add("jobNumber", typeof(int));
+        dtJobList.Columns.Add("jobName", typeof(string));
+        dtJobList.Columns.Add("Due", typeof(DateTime));
+        dtJobList.Columns.Add("Status", typeof(string));
+        DataRow drow = null;
+        #endregion
+        int i = 0;
+        StringBuilder html = new StringBuilder();
+        foreach (var x in o["jobs"])
+        {
+            drow = dtJobList.NewRow();
+            dtJobList.Rows.Add(drow);
+            dtJobList.Rows[i]["Client"] = (string)(x["client"]);
+            dtJobList.Rows[i]["jobNumber"] = (int)(x["job-number"]);
+            dtJobList.Rows[i]["jobName"] = (string)(x["job-name"]);
+            dtJobList.Rows[i]["Due"] = (DateTime)(x["due"]);
+            dtJobList.Rows[i]["Status"] = (string)(x["status"]);
+            i++;
+        }
+
+        foreach (DataRow row in dtJobList.Rows)
+        {
+            html.Append("<tr>");
+            foreach (DataColumn column in dtJobList.Columns)
+            {
+                html.Append("<td>");
+                html.Append(row[column.ColumnName]);
+                html.Append("</td>");
+            }
+            html.Append("</tr>");
+        }
+        this.placeHolder.Controls.Add(new Literal { Text = html.ToString() });
+    }
+
+    public class Job
+    {
+        public string client { get; set; }
+        public int jobnumber { get; set; }
+        public string jobname { get; set; }
+        public string due { get; set; }
+        public string status { get; set; }
+    }
+
+    public class Jobs
+    {
+        public List<Job> jobs { get; set; }
+    }
+    public string getJobsContent(string url)
+    {
+        string output = "";
+        try
+        {
+            Uri uri = new Uri(url);
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(uri);
+            request.Method = WebRequestMethods.Http.Get;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            output = reader.ReadToEnd();
+            response.Close();
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+        return output;
+    }
+}
+
